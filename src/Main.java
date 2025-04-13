da0d68666a3b39e1d7354bf6209b1656b518ce60
MarlonVieira <marlongabriel_100@hotmail.com>
2025-04-13 13:42:52 -0300

    Update

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the SreenMatch!");
+        System.out.println("Movie: Tog Gun: Maverick");
+
+        int releaseYear = 2022;
+        boolean included = false;
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Included: " + included);
+
+        double averageGrade = (9.8 + 6.3 + 8.0) /3;
+        System.out.println("Average Grade: " + averageGrade);
+
+        String synopsis;
+        synopsis = "Movie of Action and adventure with Tom Cruise.";
+        System.out.println("Synopsis: " + synopsis);
+
+        int classification = (int) (averageGrade /2);
+        System.out.println("Classification: " + classification);
+    }
+}
